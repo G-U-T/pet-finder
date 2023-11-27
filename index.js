4e02f92e92f4171b84543bb93c27fd72d87e8d20
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-27 15:37:39 -0500

    Can retrieve pet by name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 const express = require('express');
 const data = require('./data');
 
-const PORT = 7777;
+const PORT = 3345;
 const app = express();
 app.listen(PORT, () => {
 	console.log(`Port ${PORT}`);
@@ -16,4 +16,18 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/v1/pets', (req, res) => {
+	res.send(data);
+});
+
+app.get('/api/v1/pets/:name', (req, res) => {
+	let petFromName;
+	data.forEach((pet) => {
+		if (pet.name === req.params.name) petFromName = pet;
+	});
+	res.send(
+		petFromName
+	);
 });
