15f63a930b91ca1342ca73f883588033b30fe436
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-27 15:24:28 -0500

    Added pseudocode

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/* PSEUDOCODE:
+1. Make /api/v1/pets route and send the pets to the browser.
+2. Make /api/v1/pets/:name, get name from parameters and view it.
+3. Make /api/v1/pets/owner, and use req.query() to view the pet owned by that owner.
+4. Make an HTML file to be viewed at /.
+*/
+
 const express = require('express');
 const data = require('./data');
 
